5228451db6d7c3c50b58b25b8f2e49f5203e455e
Romit Modi <modiromit7@gmail.com>
2021-08-09 23:08:17 +0530

    schedule executor service change

--- src/main/java/org/romit/learning/concurrency/CleaningScheduleApp.java
+++ src/main/java/org/romit/learning/concurrency/CleaningScheduleApp.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 public class CleaningScheduleApp {
     public static void main(String[] args) {
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-        service.schedule(new CleaningScheduleRunnable(), 5, TimeUnit.SECONDS);
+        //service.schedule(new CleaningScheduleRunnable(), 5, TimeUnit.SECONDS);
+        //service.scheduleAtFixedRate(new CleaningScheduleRunnable(), 5, 4, TimeUnit.SECONDS);
+        service.scheduleWithFixedDelay(new CleaningScheduleRunnable(), 5, 4, TimeUnit.SECONDS);
     }
 }
